9a8f60081704704242fbf67b3f37808747237953
guilherme2008 <guilhermepc2008@gmail.com>
2026-06-11 14:37:55 -0300

    Refector: Reformando o método de requisição e buscando na main

--- src/Buscando.java
+++ src/Buscando.java
@@ -1,6 +1,22 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Buscando {
-    public TransfeirDados buscando(String nomedaCidade){
+    private Gson gson = new Gson();
+    public TransfeirDados buscando(String nomedaCidade) throws Exception{
         String chave = System.getenv("API");
+
         String end = "https://api.openweathermap.org/data/2.5/weather?q=" +  nomedaCidade+  "&appid=" + chave +"&units=metric&lang=pt_br";
+        HttpClient carteiro = HttpClient.newHttpClient();
+        HttpRequest pacote = HttpRequest.newBuilder().uri(URI.create(end)).build();
+        HttpResponse<String> retorno= carteiro.send(pacote, HttpResponse.BodyHandlers.ofString());
+        TransfeirDados t = gson.fromJson(retorno.body(), TransfeirDados.class);
+        System.out.println(t.toString());
+        return t;
+
     }
 }
--- src/principal.java
+++ src/principal.java
@@ -1,7 +1,9 @@
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class principal {
@@ -9,6 +11,10 @@ public class principal {
        Scanner leitura = new Scanner(System.in);
        String clima;
        System.out.println("BEM VINDO AO SISTEMA - CLIMEX");
+       System.out.println("DÍGITE O NOME DA CIDADE QUE VOCÊ DESEJA BUSCAR");
+       clima = URLEncoder.encode(leitura.nextLine(), StandardCharsets.UTF_8);
+       Buscando busca = new Buscando();
+       busca.buscando(clima);
 
 
     }
