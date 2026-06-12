25da5ec85b44398dc87341c5369a57f07b7016fc
guilherme2008 <guilhermepc2008@gmail.com>
2026-06-12 08:39:59 -0300

    feat: Adicionando menu

---
+++ src/ErronaBusca.java
@@ -0,0 +1,11 @@
+public class ErronaBusca extends RuntimeException {
+    private String msg;
+    public ErronaBusca(String message) {
+        this.msg = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.msg;
+    }
+}
